2ef3fd63754f248b1fbafa4acab08303f593145b
Rayeed Anwar <_rayeed@live.com>
2023-04-01 14:46:25 +0100

    Adding unit tests

---
+++ packages/core/test/cost.test.js
@@ -0,0 +1,29 @@
+import { expect, test } from "vitest";
+import { calculateCost } from "../src/cost";
+
+test("Lowest tier", () => {
+  const storage = 10;
+
+  const expectedCost = 4000;
+  const cost = calculateCost(storage);
+
+  expect(cost).toEqual(expectedCost);
+});
+
+test("Middle tier", () => {
+  const storage = 100;
+
+  const expectedCost = 20000;
+  const cost = calculateCost(storage);
+
+  expect(cost).toEqual(expectedCost);
+});
+
+test("Highest tier", () => {
+  const storage = 101;
+
+  const expectedCost = 10100;
+  const cost = calculateCost(storage);
+
+  expect(cost).toEqual(expectedCost);
+});
